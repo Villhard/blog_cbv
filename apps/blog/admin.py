e556221c4f344f9e0bdb7ece68ef344efa4b92d6
Victor Ryabov <victorwillhard@gmail.com>
2024-10-30 16:55:38 +0300

    feat: add Category and Post models with MPTT support, update admin and settings

--- apps/blog/admin.py
+++ apps/blog/admin.py
@@ -1,4 +1,11 @@
 from django.contrib import admin
-from .models import Post
+from django_mptt_admin.admin import DjangoMpttAdmin
+from .models import Post, Category
+
+
+@admin.register(Category)
+class CategoryAdmin(DjangoMpttAdmin):
+    prepopulated_fields = {"slug": ("title",)}
+
 
 admin.site.register(Post)
